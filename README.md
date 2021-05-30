46470aa715e84a5f648a6da3a8314b4a68361c29
vkumar <vkumar01.dev@gmail.com>
2021-05-30 22:55:55 +0530

    Initial Commit, created project.py with function1

---
+++ README.md
@@ -0,0 +1,8 @@
+# This is a test repo
+---
+
+## Usage
+To run the python files use the following command
+```
+python <file_name>.py
+```
---
+++ project.py
@@ -0,0 +1,7 @@
+
+def function1():
+    return "This is just a function which have returned you this. ;)"
+
+
+if __name__ == "__main__":
+    print(function1())
